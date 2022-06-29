62f983e1e8ae8ed964abef0cb979308641ca4a3f
Aqueeb99 <enggaqueeb@gmail.com>
2022-06-29 20:06:09 +0530

    updated

--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,5 +1,6 @@
 const internModel = require("../models/internModel")
 const mongoose = require('mongoose')
+const collegeModel = require("../models/collegeModel")
 
 // --------------------------- Regex for email and mobile ---------------------
 
@@ -11,6 +12,11 @@ const mobileRegex = /^[6-9]\d{9}$/
 
 //
 const isValidObjectId = function (objectId) { return mongoose.Types.ObjectId.isValid(objectId) }
+const objectValue = function (value) {
+    if (typeof value === "undefined" || value === null || typeof value === Number) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    return true
+}
 
 
 // -------------------------- CREATE Intern -----------------------------
@@ -18,22 +24,25 @@ const isValidObjectId = function (objectId) { return mongoose.Types.ObjectId.isV
 const createIntern = async function (req, res) {
     try {
         const internData = req.body
-
+        const { name, email, mobile, collegeName } = internData
         console.log(internData)
 
         if (Object.keys(internData).length == 0) {
             return res.status(400).send({ status: false, msg: "Please Provide Data" })
         }
 
-        // if (!internData.name) {
-        //     return res.status(400).send({ status: false, msg: "Please Provide Name" })
-        // }
+        if (!internData.name) {
+            return res.status(400).send({ status: false, msg: "Please Provide Name" })
+        }
+        if (objectValue(internData.name)) {
+            return res.status(400).send({ status: false, msg: "Please Provide valid Name" })
+        }
         if (!internData.email) {
             console.log(internData.email)
             return res.status(400).send({ status: false, msg: "Please Provide email" })
         }
-        if (!emailRegex.test(internData.email)) 
-        return res.status(400).send({ status:false,  message: "Please Enter Email in valid Format" })
+        if (!emailRegex.test(internData.email))
+            return res.status(400).send({ status: false, message: "Please Enter Email in valid Format" })
 
         let duplicateEmail = await internModel.findOne({ email: internData.email });
         if (duplicateEmail) {
@@ -50,14 +59,18 @@ const createIntern = async function (req, res) {
             return res.status(400).send({ status: false, msg: `${internData.mobile} already exists!` });
         }
 
-        if (!internData.collegeId) {
-            return res.status(400).send({ status: false, msg: "Please Provide college Id" })
-        }
-        // if(!isValidObjectId(internData.collegeId)){
-        //     return res.status(400).send({ status: false, msg: "Please Provide valid college Id" })
-        // }
 
-        let savedData = await internModel.create(internData)
+        if (!internData.collegeName) {
+            return res.status(400).send({ status: false, msg: "Please Provide college name" })
+
+        }
+        const collegeData = await collegeModel.findOne({ name: collegeName })
+        if (!collegeData) {
+            return res.status(400).send({ status: false, msg: "college data not found" })
+        }
+        const collegeId = collegeData._id // 
+        const dataOfIntern = { name, email, mobile, collegeId } // restructuring of internData
+        let savedData = await internModel.create(dataOfIntern)
         return res.status(201).send({ status: true, data: savedData })
     }
     catch (err) {
